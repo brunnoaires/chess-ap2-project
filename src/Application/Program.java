6b305be0969934ff9ce5c4e354738edf045ac1e0
Brunno <brunnoaires26@hotmail.com>
2021-10-18 01:31:33 -0300

    adicionado o printing do tabuleiro e a pasta chess para criaçao e funcionalidade do sistema

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -2,12 +2,17 @@ package Application;
 
 import Boardgame.Board;
 import Boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
        public static void main (String[] args) {
 
            Board board = new Board(8, 8);
+
+
+           ChessMatch chessMatch = new ChessMatch();
+           UI.printBoard(chessMatch.getPieces());
 
 
 
